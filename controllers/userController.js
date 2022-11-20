05c076e0005c98fb5f7721acd5aca05431fbbfb0
PURUSHOTHAM REDDY <84611950+PURUSHOTHAM-REDDY-N@users.noreply.github.com>
2022-11-20 06:45:24 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -72,7 +72,9 @@ module.exports.register=async (req,res,next) => {
 
     res.cookie("jwt",token,{
         withCredentials:true,
+        secure:true,
         httpOnly:false,
+        sameSite:"none"
         maxAge:maxAge*1000});
         
         res.status(201).json({user:user._id,created:true})
